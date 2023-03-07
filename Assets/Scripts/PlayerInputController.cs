3e89a3744365cf1fd989b3a6cac547d3d9d61d2b
Mitch McClellan <mitchelljmcclellan@gmail.com>
2023-03-06 20:07:39 -0500

    Refactored PlayerInputController to support use of WASD or Numpad controls

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -33,48 +33,49 @@ public class PlayerInputController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        InputCheck();
+        InputCheckNumpad();
+        InputCheckWASD();
     }
 
-    void InputCheck()
+    void InputCheckNumpad()
     {
         if (Input.GetKeyDown(KeyCode.Keypad5))
         {
-            playerActor.Move(new Vector3Int(0, 0));
+            PlayerWait();
         }
         if (Input.GetKeyDown(KeyCode.Keypad6))
         {
-            playerActor.Move(new Vector3Int(1, 0));
+            PlayerMoveRight();
         }
         if (Input.GetKeyDown(KeyCode.Keypad4))
         {
-            playerActor.Move(new Vector3Int(-1, 0));
+            PlayerMoveLeft();
         }
         if (Input.GetKeyDown(KeyCode.Keypad2))
         {
-            playerActor.Move(new Vector3Int(0, -1));
+            PlayerMoveDown();
         }
         if (Input.GetKeyDown(KeyCode.Keypad8))
         {
-            playerActor.Move(new Vector3Int(0, 1));
+            PlayerMoveUp();
         }
 
 
         if (Input.GetKeyDown(KeyCode.Keypad9))
         {
-            playerActor.Move(new Vector3Int(1, 1));
+            PlayerMoveUpRight();
         }
         if (Input.GetKeyDown(KeyCode.Keypad7))
         {
-            playerActor.Move(new Vector3Int(-1, 1));
+            PlayerMoveUpLeft();
         }
         if (Input.GetKeyDown(KeyCode.Keypad1))
         {
-            playerActor.Move(new Vector3Int(-1, -1));
+            PlayerMoveDownLeft();
         }
         if (Input.GetKeyDown(KeyCode.Keypad3))
         {
-            playerActor.Move(new Vector3Int(1, -1));
+            PlayerMoveDownRight();
         }
 
         if (Input.GetKeyDown(KeyCode.KeypadPlus))
@@ -90,20 +91,135 @@ public class PlayerInputController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.KeypadDivide))
         {
-            if (InventoryMenu.activeInHierarchy)
+            PlayerOpenInventory();
+        }
+    }
+
+    void InputCheckWASD()
+    {
+        // Apparently to get a combo of keys to work you have to do GetKey1, GetKey2, ..., GetFinalKeyDown
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.D)
+                || Input.GetKey(KeyCode.D) && Input.GetKeyDown(KeyCode.W))
             {
-                InventoryMenu.SetActive(false);
+                PlayerMoveUpRight();
             }
-            else
+            if (Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.A)
+                || Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.W))
             {
-                InventoryMenu.SetActive(true);
+                PlayerMoveUpLeft();
             }
+            if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.A)
+                || Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.S))
+            {
+                PlayerMoveDownLeft();
+            }
+            if (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.D)
+                || Input.GetKey(KeyCode.D) && Input.GetKeyDown(KeyCode.S))
+            {
+                PlayerMoveDownRight();
+            }
+            return;
         }
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PlayerWait();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            PlayerMoveRight();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayerMoveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            PlayerMoveDown();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            PlayerMoveUp();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            playerActor.GoDownStairs();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("TEST");
+            playerActor.Interact();
+        }
+
+        if (Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.Tab))
+        {
+            PlayerOpenInventory();
+        }
     }
 
     public Vector3Int GetLocation()
     {
         return playerActor.gridPosition;
+    }
+
+    private void PlayerWait()
+    {
+        playerActor.Move(new Vector3Int(0, 0));
+    }
+
+    private void PlayerMoveRight()
+    {
+        playerActor.Move(new Vector3Int(1, 0));
+    }
+
+    private void PlayerMoveLeft()
+    {
+        playerActor.Move(new Vector3Int(-1, 0));
+    }
+
+    private void PlayerMoveDown()
+    {
+        playerActor.Move(new Vector3Int(0, -1));
+    }
+
+    private void PlayerMoveUp()
+    {
+        playerActor.Move(new Vector3Int(0, 1));
+    }
+
+    private void PlayerMoveUpRight()
+    {
+        playerActor.Move(new Vector3Int(1, 1));
+    }
+
+    private void PlayerMoveUpLeft()
+    {
+        playerActor.Move(new Vector3Int(-1, 1));
+    }
+
+    private void PlayerMoveDownLeft()
+    {
+        playerActor.Move(new Vector3Int(-1, -1));
+    }
+
+    private void PlayerMoveDownRight()
+    {
+        playerActor.Move(new Vector3Int(1, -1));
+    }
+
+    private void PlayerOpenInventory()
+    {
+        if (InventoryMenu.activeInHierarchy)
+        {
+            InventoryMenu.SetActive(false);
+        }
+        else
+        {
+            InventoryMenu.SetActive(true);
+        }
     }
 }
