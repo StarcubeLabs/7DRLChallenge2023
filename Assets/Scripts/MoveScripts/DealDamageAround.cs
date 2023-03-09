2fbcce374920d895f0d179c7ae6bac9f741140a7
Cheng Hann Gan <chenghann_gan@ultimatesoftware.com>
2023-03-09 13:23:47 -0500

    Added Explosion move

---
+++ Assets/Scripts/MoveScripts/DealDamageAround.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DealDamageAround : MoveData
+{
+    private Vector3Int[] AROUND_OFFSETS =
+    {
+        Vector3Int.up,
+        new Vector3Int(1, 1),
+        Vector3Int.right,
+        new Vector3Int(1, -1),
+        Vector3Int.down,
+        new Vector3Int(-1, -1),
+        Vector3Int.left,
+        new Vector3Int(-1, 1)
+    };
+    
+    public override void UseMove(ActorController user)
+    {
+        foreach (Vector3Int offset in AROUND_OFFSETS)
+        {
+            ActorController target = ServicesManager.EntityManager.getEntityInPosition(user.gridPosition + offset);
+            if (target && !target.Dead)
+            {
+                DamageTarget(user, target);
+            }
+        }
+    }
+}
