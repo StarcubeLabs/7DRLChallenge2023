aa293f3c8e5945f6ecfd776977fbcb25f643f74d
Spex130 <Gamerguy56@bellsouth.net>
2023-03-11 19:02:02 -0500

    Cure and Explosion and Shield

--- Assets/Scripts/MoveScripts/ApplyStatusSelfMove.cs
+++ Assets/Scripts/MoveScripts/ApplyStatusSelfMove.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class ApplyStatusSelfMove : MoveData
 {
     public override void UseMove(ActorController user)
@@ -13,5 +15,13 @@ public class ApplyStatusSelfMove : MoveData
     public override bool InAIRange(ActorController user, ActorController target)
     {
         return true;
+    }
+
+    public override void TriggerVFX(ActorController user)
+    {
+        if (launchVFX != null)
+        {
+            GameObject.Instantiate<ParticleSystem>(launchVFX, user.transform, false);
+        }
     }
 }
